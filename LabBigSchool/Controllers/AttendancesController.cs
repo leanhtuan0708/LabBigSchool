5ec5d5a1a19658e3d707d108f9ec6a9f2afee042
Leanhtuan0708 <bentele80000@gmail.com>
2021-07-07 16:04:56 +0700

    Update-07/07/2021

---
+++ LabBigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using LabBigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabBigSchool.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public AttendancesController() 
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId) 
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseId))
+                return BadRequest("The Attendance already axists!");
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();        
+        }
+
+    }
+}
